5e6ef42631a2be2b9c449996bfa662757a17cab4
yangjj <yangjj@gzsendi.cn>
2024-09-28 14:01:53 +0800

    react持久化适配微信小程序

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,8 +1,31 @@
 import { init, RematchDispatch, RematchRootState } from '@rematch/core'
 import { models, RootModel } from '@/models'
 import persistPlugin from "@rematch/persist";
-import storage from "redux-persist/lib/storage";
-
+import { getStorageSync,setStorageSync,removeStorageSync } from '@tarojs/taro';
+const storage = {  
+    getItem: (key:string) => {  
+      try {  
+        return getStorageSync(key);  
+      } catch (e) {  
+        console.error(e);  
+      }  
+    },  
+    setItem: (key:string, value:string) => {  
+      try {  
+        setStorageSync(key, value);  
+      } catch (e) {  
+        console.error(e);  
+      }  
+    },  
+    removeItem: (key:string) => {  
+      try {  
+        removeStorageSync(key);  
+      } catch (e) {  
+        console.error(e);  
+      }  
+    },  
+  };  
+  
 const persist_plugin = persistPlugin<RootModel, RootModel>({
     whitelist: [
     ],
